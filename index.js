881cbd5f2fd81035801d8aa14554a9be1207ecd3
EmanueleDB <leleoioioi@gmail.com>
2020-02-05 10:18:34 +0100

    drawing rectangles

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  Mouse,
+  MouseConstraint
+} = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -12,8 +20,19 @@ const render = Render.create({
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+  })
+);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+//Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+World.add(world, walls);
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
